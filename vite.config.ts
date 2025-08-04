ad7a8b536c510fb5798359f7863b50ea4fa4e771
utkarsh-2033 <utkarshg@2033>
2025-08-04 22:19:00 +0530

    minor updates

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/api',
+        target: 'https://clipiq.vercel.app/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
